2e04da755aa10c39ce614af1a7f6b0a5993ed6cf
Vasile Ropot <ropotvs@gmail.com>
2026-07-27 16:51:38 +0300

    font setup
    Signed-off-by: Vasile Ropot <ropotvs@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,19 @@
+import { fontSpaceMono } from '@telex/fonts';
 import type { Metadata } from 'next';
 import { ReactNode } from 'react';
-import './globals.css';
+import './layout.css';
 
 export const metadata: Metadata = {
-  title: 'Messenger',
-  description: 'A lightweight messaging app',
+  title: 'Telex',
+  description: 'A small message board',
 };
 
 export default function RootLayout(props: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <body className="flex min-h-full flex-col">{props.children}</body>
+    <html className={fontSpaceMono.variable} lang="en">
+      <body className="flex min-h-full flex-col font-mono">
+        {props.children}
+      </body>
     </html>
   );
 }
---
+++ libs/lib-fonts/font-space-mono.ts
@@ -0,0 +1,7 @@
+import { Space_Mono } from 'next/font/google';
+
+export const fontSpaceMono = Space_Mono({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-space-mono',
+});
