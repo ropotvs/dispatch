28084245b6a9bfa114e82804a21be2b0021da5a8
Vasile Ropot <ropotvs@gmail.com>
2026-07-27 19:45:26 +0300

    export atoms
    Signed-off-by: Vasile Ropot <ropotvs@gmail.com>

--- libs/lib-atoms/index.ts
+++ libs/lib-atoms/index.ts
@@ -1,4 +1,6 @@
+export * from './atom-avatar';
 export * from './atom-button';
 export * from './atom-input';
 export * from './atom-label';
 export * from './atom-logo';
+export * from './atom-tag';
