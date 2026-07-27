3bbded88f9b1fd4a1ba8d7b19f5b8f8ad0da4675
Vasile Ropot <ropotvs@gmail.com>
2026-07-27 22:40:32 +0300

    support for atom-button with custom element
    Signed-off-by: Vasile Ropot <ropotvs@gmail.com>

--- libs/lib-atoms/atom-button.tsx
+++ libs/lib-atoms/atom-button.tsx
@@ -1,15 +1,29 @@
 import { clsx } from 'clsx';
-import { ComponentProps } from 'react';
+import {
+  cloneElement,
+  ComponentProps,
+  MouseEventHandler,
+  ReactElement,
+  ReactNode,
+} from 'react';
 
-export function AtomButton(props: ComponentProps<'button'>) {
-  return (
-    <button
-      type="button"
-      {...props}
-      className={clsx(
-        'border-ink bg-brand inline-flex h-13.5 cursor-pointer items-center justify-center gap-2 border-[3px] font-mono text-base font-bold shadow-[5px_5px_0_var(--color-ink)] lg:h-14 lg:tracking-[0.02em]',
-        props.className,
-      )}
-    />
-  );
+export function AtomButton(props: {
+  className?: string;
+  disabled?: boolean;
+  type?: 'button' | 'submit';
+  children?: ReactNode;
+  element?: ReactElement<ComponentProps<'button'>>;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}) {
+  return cloneElement(props.element ?? <button />, {
+    ...props.element?.props,
+    children: props.children,
+    className: clsx(
+      'border-ink bg-brand inline-flex h-13.5 cursor-pointer items-center justify-center gap-2 border-[3px] font-mono text-base font-bold shadow-[5px_5px_0_var(--color-ink)] lg:h-14 lg:tracking-[0.02em]',
+      props.className,
+    ),
+    disabled: props.disabled,
+    onClick: props.onClick,
+    type: props.element ? undefined : (props.type ?? 'button'),
+  });
 }
