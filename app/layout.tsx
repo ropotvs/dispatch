ce6d4d18b514cb0bdfef0b2e78e946f256ad4f0c
Vasile Ropot <ropotvs@gmail.com>
2026-07-27 16:26:24 +0300

    initialize prettier
    Signed-off-by: Vasile Ropot <ropotvs@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import {ReactNode} from "react";
+import type { Metadata } from 'next';
+import { ReactNode } from 'react';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "Messenger",
-  description: "A lightweight messaging app",
+  title: 'Messenger',
+  description: 'A lightweight messaging app',
 };
 
 export default function RootLayout(props: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className="min-h-full flex flex-col">{props.children}</body>
+      <body className="flex min-h-full flex-col">{props.children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      app
-    </div>
-  );
+  return <div>app</div>;
 }
