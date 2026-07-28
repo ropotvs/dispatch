0cb79e77a66e0b3cf85e28465ff2af85ab217e38
Vasile Ropot <ropotvs@gmail.com>
2026-07-28 11:49:00 +0300

    introduce auth login form
    Signed-off-by: Vasile Ropot <ropotvs@gmail.com>

---
+++ libs/lib-types/type-form-auth-login.ts
@@ -0,0 +1,7 @@
+import { TypeFieldEmail } from './type-field-email';
+import { TypeFieldPassword } from './type-field-password';
+
+export type TypeFormAuthLogin = {
+  email: TypeFieldEmail;
+  password: TypeFieldPassword;
+};
