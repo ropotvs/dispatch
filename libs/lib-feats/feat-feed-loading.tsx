f592d942e7735557f45e0340c1b687a007969b2c
Vasile Ropot <ropotvs@gmail.com>
2026-07-28 22:26:13 +0300

    simplify loading feed component
    Signed-off-by: Vasile Ropot <ropotvs@gmail.com>

--- libs/lib-feats/feat-feed-loading.tsx
+++ libs/lib-feats/feat-feed-loading.tsx
@@ -1,4 +1,5 @@
-import { FeatFeedMessageLoading } from './feat-feed-message-loading';
+import { AtomSkeleton } from '@dispatch/atoms';
+import { clsx } from 'clsx';
 
 export function FeatFeedLoading() {
   return (
@@ -17,20 +18,35 @@ export function FeatFeedLoading() {
           text: 'w-[95%]',
         },
         {
-          className: 'hidden lg:block',
           handle: 'w-17.5',
           name: 'w-32.5',
           text: 'w-[88%]',
         },
       ].map((skeleton) => (
-        <FeatFeedMessageLoading
-          className={skeleton.className}
-          handle={skeleton.handle}
+        <div
           key={skeleton.name}
-          line2={skeleton.line2}
-          name={skeleton.name}
-          text={skeleton.text}
-        />
+          className="border-ink border-[3px] bg-white p-3.5 lg:p-4.5"
+        >
+          <div className="flex items-center gap-2 lg:gap-2.5">
+            <AtomSkeleton className="size-8 lg:size-9.5" />
+            <div className="flex-1">
+              <AtomSkeleton className={clsx('h-3', skeleton.name ?? 'w-35')} />
+              <AtomSkeleton
+                className={clsx(
+                  'mt-2 hidden h-2.5 lg:block',
+                  skeleton.handle ?? 'w-22.5',
+                )}
+              />
+            </div>
+          </div>
+          <AtomSkeleton
+            className={clsx('mt-3 h-3 lg:mt-4', skeleton.text ?? 'w-full')}
+          />
+          {skeleton.line2 && (
+            <AtomSkeleton className={clsx('mt-2 h-3', skeleton.line2)} />
+          )}
+          <AtomSkeleton className="mt-3 h-5 w-19 lg:mt-4 lg:h-5.5 lg:w-22.5" />
+        </div>
       ))}
     </>
   );
