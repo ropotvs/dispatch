413696c068243828ccb9f28c42c97c162449cda5
Vasile Ropot <ropotvs@gmail.com>
2026-07-28 12:44:57 +0300

    integrate new fields into auth-login form
    Signed-off-by: Vasile Ropot <ropotvs@gmail.com>

--- libs/lib-forms/form-auth-login.tsx
+++ libs/lib-forms/form-auth-login.tsx
@@ -4,34 +4,40 @@ import { AtomButton } from '@dispatch/atoms';
 import { FieldEmail, FieldPassword } from '@dispatch/fields';
 import { IconArrowRight } from '@dispatch/icons';
 import { TypeFormAuthLogin } from '@dispatch/types';
-import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 export function FormAuthLogin(props: {
   className?: string;
   defaults: TypeFormAuthLogin;
   onSubmit: SubmitHandler<TypeFormAuthLogin>;
 }) {
-  const form = useForm<TypeFormAuthLogin>({ defaultValues: props.defaults });
+  const form = useForm<TypeFormAuthLogin>({
+    defaultValues: props.defaults,
+  });
 
   return (
-    <FormProvider {...form}>
-      <form
-        className={props.className}
-        onSubmit={form.handleSubmit(props.onSubmit)}
-      >
-        <FieldEmail label="Email" name="email" placeholder="ada@dispatch.dev" />
-        <div className="mt-4.5 lg:mt-5">
-          <FieldPassword
-            label="Password"
-            name="password"
-            placeholder="••••••••"
-            autocomplete="current-password"
-          />
-        </div>
-        <AtomButton type="submit" className="mt-7 w-full lg:mt-8">
-          LOG IN <IconArrowRight />
-        </AtomButton>
-      </form>
-    </FormProvider>
+    <form
+      className={props.className}
+      onSubmit={form.handleSubmit(props.onSubmit)}
+    >
+      <FieldEmail
+        control={form.control}
+        label="Email"
+        name="email"
+        placeholder="ada@dispatch.dev"
+      />
+      <div className="mt-4.5 lg:mt-5">
+        <FieldPassword
+          control={form.control}
+          label="Password"
+          name="password"
+          placeholder="••••••••"
+          autocomplete="current-password"
+        />
+      </div>
+      <AtomButton type="submit" className="mt-7 w-full lg:mt-8">
+        LOG IN <IconArrowRight />
+      </AtomButton>
+    </form>
   );
 }
